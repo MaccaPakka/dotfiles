c0cb2b36775290fe72735e88432f3e50e726040c
Stevie McCann <steviemccann@rentalcars.com>
2020-02-15 22:04:13 +0000

    feat: add .npm.sh script

---
+++ .npm.sh
@@ -0,0 +1,10 @@
+# Install global Node ackages
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install useful global packages
+npm i browser-sync -g
